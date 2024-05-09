2f0986033c436655de69159a2e02a6510ba0f791
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-05-09 14:21:04 +0000

    python 09.05

--- Exercícios/09-05 - idade, sexo, salário.py
+++ Exercícios/09-05 - idade, sexo, salário.py
@@ -1,45 +1,66 @@
 import os
-os.system("cls||clear")
+import sys #trás o maior valor inteiro ou menor
 
-menorIdade = 999
+os.system("cls || clear")
+
 maiorIdade = 0
-salario = 0
-quantidadeSalario = 0
-somaSalario = 0
+menorIdade = sys.maxsize
 mulheres5k = 0
-
+somaSalarios = 0
+quantidadeSalarios = 0
+mediaSalarios = 0
 
 while True:
     print("Código \t Descrição")
     print("1 \t Adicionar pessoa")
     print("2 \t Exibir resultados e sair")
+    opcao = int(input("Digite a opção desejada: "))
+   
+    match(opcao):
+        case 1:
+            print("= = = Solicitando dados = = =")
 
-    resposta = int(input(f"Digite a opção desejada: "))
+            idade = int(input("Digite a idade: "))
+            
+            while idade < 0:
+                print("Idade inválida! Digite novamente.")
+                idade = int(input("Digite a idade: "))
+                
 
-    if resposta == 1:
-        idade = int(input(f"Digite sua idade"))
-        sexo = input(f"Digite seu sexo (M ou F): ")
-        sexo = sexo.upper()
-        salario = float(input(f"Digite seu salário: R$"))
+            sexo = input("Digite o sexo (M ou F): ")
+            sexo = sexo.upper()
+        
+            while sexo != "F" and sexo != "M":
+                print("Digite a letra correspondente ao seu sexo.")
+                sexo = input("Digite o sexo (M ou F): ")
+                sexo = sexo.upper()
+                
+            
+            salario = float(input("Digite o salário: R$"))
+            
+            while salario < 0:
+                print("Digite seu salário com um valot positivo.")
+                salario = float(input("Digite o salário: R$"))
+                
 
-        if idade > maiorIdade:
-            maiorIdade = idade
+            somaSalarios += salario
+            quantidadeSalarios += 1
 
-        if idade < menorIdade:
-            menorIdade = idade            
+            maiorIdade = max(idade, maiorIdade)
+            menorIdade = min(idade, menorIdade)
 
-        if sexo == "F" and salario >= 5000:
-            mulheres5k += 1
+            if sexo == "F" and salario >= 5000:
+                mulheres5k += 1
+
+            if quantidadeSalarios != 0:
+                mediaSalarios = somaSalarios / quantidadeSalarios
+       
+        case 2:
+            print("= = = Mostrando resultados = = =")
+            print(f"Média de salários do grupo: R${mediaSalarios}")
+            print(f"Maior idade do grupo: {maiorIdade}")
+            print(f"Menor idade do grupo: {menorIdade}")
+            print(f"Mulheres com salário a partir de 5 mil: {mulheres5k}")
             break
-
-    elif resposta == 2:
-        mediaSalarial = somaSalario / quantidadeSalario
-
-        print(f"Média salárial do grupo: R${mediaSalarial}")        
-        print(f"Menor Idade: {maiorIdade}")        
-        print(f"Maior Idade: {menorIdade}")        
-        print(f"Mulheres com salário acima de 5000: {mulheres5k}")
-        break
-
-    else:
-        print("Opção inválida!")          
+        case _:
+            print("Opção inválida. \nTente novamente.")
