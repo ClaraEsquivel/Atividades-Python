e4f0783c0bb3c620775ce04a40963d1ac16ea822
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-05-23 14:20:12 +0000

    função 23-05

---
+++ Exercícios/23-05 - inflação (função).py
@@ -0,0 +1,26 @@
+import os
+os.system("cls|clear")
+
+def logoSenai():
+    os.system("cls|clear")
+    print("=== = = = = = = = = = ===")
+    print("= = = SENAI | FIEB = = =")
+    print("=== = = = = = = = = = ===")
+
+
+def inflacionar(valor):
+    novoValor = 0
+    if valor < 100:
+        novoValor = valor * 1.1
+    else:
+        novoValor = valor * 1.2    
+    return novoValor
+
+
+logoSenai()
+valor = float(input(f"Digite o valor do produto: "))
+novoValor = inflacionar(valor)
+
+logoSenai()
+print(f"Novo valor: {novoValor:.2f1}")
+
